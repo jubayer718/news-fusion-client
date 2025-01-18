8778e3b463dfeb9c205e957460c01c7ed023bdc8
jubayer718 <jubayerahmed2462@gmail.com>
2025-01-18 07:23:05 +0600

    filter and search functionality complete

--- src/AllComponents/AllArticles/AllArticles.jsx
+++ src/AllComponents/AllArticles/AllArticles.jsx
@@ -3,37 +3,38 @@ import useAxiosPublic from "../../axiosPublic/UseAxiosPublic";
 import useApprovedArticles from "../../Hooks/useApprovedArticles";
 import ArticlesCard from "./allArticlesCard/ArticlesCard";
 import usePublisher from "../../Hooks/usePublisher";
-
+import { Controller, useForm} from 'react-hook-form';
+import Select from 'react-select';
 
 const AllArticles = () => {
   const [filter, setFilter] = useState('');
   const [search, setSearch] = useState('');
-
-  const [approvedArticles, refetch] = useApprovedArticles(filter,search);
+  const [tags, setTags] = useState([]);
+  const [approvedArticles, refetch] = useApprovedArticles(filter,search,tags.map(tag=>tag.value));
   const [publisher]=usePublisher()
-  // console.log(publisher);
-  // const [article, setArticle] = useState([])
  
-  // // filter article data by title and tag
-  // useEffect(() => {
-  //   const fetchAllJobs = async () => {
-  //   const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/all-jobs?filter=${filter}&search=${search}&sort=${sort}`)
-  //   setJobs(data)
-  // }
-  //   fetchAllJobs()
-  // }, [filter,search,sort])
 
   const handleReset = () => {
     setFilter('');
-    
       setSearch('');
   }
 
-
-
+ const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    control
+  } = useForm()
+ const tagOptions = [
+    { value: "Technology", label: "Technology" },
+    { value: "Health", label: "Health" },
+    { value: "Business", label: "Business" },
+    { value: "Sports", label: "Sports" },
+    { value: "Entertainment", label: "Entertainment" },
+  ];
   return (
     <div>
-         <div className='flex flex-col md:flex-row justify-center items-center gap-5 '>
+         <div className='flex flex-col md:flex-row justify-center items-center gap-5 my-12 '>
           <div>
           <select
             
@@ -48,9 +49,31 @@ const AllArticles = () => {
               
             </select>
           </div>
-
-          <form>
-            <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
+          <div>
+             
+              <Controller
+                name='tags'
+                control={control}
+                 
+                rules={{ required: 'Please select at least one tag' }}
+                render={({ field }) => (
+                  <Select
+                    {...field}
+                   onChange={(selected) => setTags(selected || [])}
+                options={tagOptions}
+                    isMulti
+                placeholder='filter by tag'
+                className="basic-multi-select"
+                classNamePrefix="select"
+              />
+                )}
+              ></Controller>
+              
+            </div>
+        <form>
+          
+          <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
+            
               <input
                 className='px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent'
                 type='text'
@@ -60,8 +83,8 @@ const AllArticles = () => {
                 placeholder='Enter article Title'
                 aria-label='Enter article Title'
               />
-
-              <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none'>
+                
+              <button className='px-1 md:px-4 py-3 text-sm font-medium tracking-wider  uppercase transition-colors duration-300 transform bg-orange-400 rounded-md hover:bg-gray-300 focus:bg-gray-600 focus:outline-none'>
                 Search
               </button>
             </div>
@@ -79,7 +102,7 @@ const AllArticles = () => {
               <option value='asc'>Ascending Order</option>
             </select>
           </div> */}
-          <button onClick={handleReset} className='btn'>Reset</button>
+          <button onClick={handleReset} className='btn bg-orange-400'>Reset</button>
         </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-3">
         {approvedArticles.map(article =>
--- src/Hooks/useApprovedArticles.jsx
+++ src/Hooks/useApprovedArticles.jsx
@@ -2,14 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosPublic from "../axiosPublic/UseAxiosPublic";
 
 
-const useApprovedArticles = (filter, search) => {
-  console.log(search);
+const useApprovedArticles = (filter, search,tags) => {
   const axiosPublic = useAxiosPublic();
   const { data: approvedArticles = [], refetch } = useQuery({
-    queryKey: ['approvedData',filter,search],
+    queryKey: ['approvedData',filter,search,tags],
     queryFn: async () => {
-      const res = await axiosPublic.get(`/allArticle/approved?filter=${filter}&search=${search}`)
-      console.log(res.data);
+      const res = await axiosPublic.get(`/allArticle/approved?filter=${filter}&search=${search}&tags=${JSON.stringify(tags)}`)
+      // console.log(res.data);
       return res.data
     }
   })
